b50ceffb48ab867aa4f6ee6babb6b6255d03937f
Nishant Singh <89241026+NishantSingh10@users.noreply.github.com>
2024-09-05 14:43:06 +0530

    Create service-export.sh

---
+++ service-export.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the processed-config.yaml file exists
+if [ ! -f "./egs-installation/egs-installer-config.yaml" ]; then
+    echo "Error: processed-config.yaml not found."
+    exit 1
+fi
+
+# Use \${EXTERNAL_IP} as a literal string
+sed -i 's|grafanaDashboardBaseUrl: "http:///d/Oxed_c6Wz"|grafanaDashboardBaseUrl: "http://${EXTERNAL_IP}/d/Oxed_c6Wz"|' ./egs-installation/egs-installer-config.yaml
