74e934f83d0ef5f3076034fc3d1be8f23b529e59
srinivas-avesha <srinivas@aveshasystems.com>
2025-09-16 16:03:48 +0530

    updating UAT charts

--- egs-installer.sh
+++ egs-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the script version
-SCRIPT_VERSION="1.15.1"
+SCRIPT_VERSION="1.15.2"
 
 # Check if the script is running in Bash
 if [ -z "$BASH_VERSION" ]; then
--- egs-preflight-check.sh
+++ egs-preflight-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the script version
-SCRIPT_VERSION="1.15.1"
+SCRIPT_VERSION="1.15.2"
 
 # Global Configuration and Constants
 readonly MAX_TIMEOUT=30
